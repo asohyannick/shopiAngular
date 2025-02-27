b6ce48e5f7644d2f082442e584e5798cd32e9f1d
asohyannick <asohyannick3@gmail.com>
2025-02-27 08:26:32 -0800

    new modifications added to the package.json file

--- src/config/databaseConfig/databaseConfig.ts
+++ src/config/databaseConfig/databaseConfig.ts
@@ -5,7 +5,6 @@ const databaseConfiguration = async () => {
     try {
         await mongoose.connect(dbURL);
         console.log("Connected to the database...");
-        process.exit(0);
     } catch (err) {
         console.error("Failed to connect to the database", err);
         process.exit(1);
